2661e0eb3b82eda30ba0db48111293da80e3ad60
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-07-05 09:38:29 +0200

    clean up after code coaches comments

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
